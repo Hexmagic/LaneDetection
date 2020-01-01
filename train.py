ba4c06e2c3be1ae1b2d318a2150dad7622f27783
zai <zai@aliyun.com>
2020-01-01 13:12:14 +0800

    add log

--- train.py
+++ train.py
@@ -1,7 +1,11 @@
+import logging
+import os
+
 import cv2
 import matplotlib.pyplot as plt
 import numpy as np
 import torch
+from logzero import setup_logger
 from torch.autograd import Variable
 from torch.nn import BCELoss, CrossEntropyLoss, NLLLoss
 from torch.optim import Adam, RMSprop
@@ -10,12 +14,19 @@ from model.deeplabv3_plus import DeeplabV3Plus
 from util.datagener import LanDataSet, get_train_loader
 from util.label_util import label_to_color_mask, mask_to_label
 
+torch.cuda.set_device(6)
 
+if not os.path.exists("log"):
+    os.mkdir("log")
+
+logger = setup_logger(
+    name="", logfile="log/loss.log", level=logging.INFO, maxBytes=1e6, backupCount=3
+)
 
 
 def train():
     model = DeeplabV3Plus(n_class=8).cuda()
-    loss_func =BCELoss().cuda()
+    loss_func = BCELoss().cuda()
     opt = Adam(params=model.parameters())
     loader = get_train_loader()
     loss_list = []
@@ -24,15 +35,14 @@ def train():
             x, y = batch
             xv, yv = Variable(x).cuda(), Variable(y).cuda()
             yhat = model(xv)
-            opt.zero_grad()
             yhat = torch.sigmoid(yhat)
             yv = torch.sigmoid(yv)
+            opt.zero_grad()
             loss = loss_func(yhat, yv)
             loss_list.append(loss.item())
-            if i % 10 == 0:
-                print(f'Epoch {epoch} loss {sum(loss_list)/len(loss_list)}')
+            logger.info(f"Loss Value {loss.item()}")
             loss.backward()
             opt.step()
 
 
-testModel()
+train()
