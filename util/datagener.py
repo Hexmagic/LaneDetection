683b5045d32037467c395842213eb12782ad63ac
Hexmagic <191440042@qq.com>
2020-01-03 13:47:10 +0800

    update

--- util/datagener.py
+++ util/datagener.py
@@ -4,17 +4,17 @@ import pandas as pd
 from torch.utils.data import DataLoader, Dataset
 import torch
 from util.label_util import mask_to_label
-from torchvision.transforms import ToTensor, Compose, Normalize
+from torchvision.transforms import ToTensor, Compose, Normalize,ColorJitter,ToPILImage
 
 
 def one_hot(img):
     zs = np.array([np.zeros_like(img) for i in range(8)], dtype=np.float32)
     for i in range(8):
-        zs[i][img == i] = i
+        zs[i][img == i] = 1
     return zs
 
 
-def crop_resize_data(image, label=None, image_size=[1024, 384], offset=690):
+def crop_resize_data(image, label=None, image_size=[1024//2, 384//2], offset=690):
     roi_image = image[offset:, :]
     if label is not None:
         roi_label = label[offset:, :]
@@ -36,9 +36,10 @@ class LanDataSet(Dataset):
     def __init__(self, root: str = "", transform=None, *args, **kwargs):
         super(LanDataSet, self).__init__(*args, **kwargs)
         self.transform = Compose(
-            [
+            [   
+                ToPILImage(),
+                ColorJitter(contrast=0.3,saturation=0.3),
                 ToTensor(),
-                Normalize(mean=[0.3173, 0.2803, 0.2497], std=[0.1696, 0.1562, 0.1345]),
             ]
         )
         self.csv = pd.read_csv(root)
@@ -49,7 +50,7 @@ class LanDataSet(Dataset):
     def __getitem__(self, index):
         row = self.csv.iloc[index]
         img, mask = row["img"], row["label"]
-        img = (cv2.imread(img) / 255.0).astype(np.float32)
+        img = cv2.imread(img)
         mask = cv2.imread(mask, 0)
         img, mask = crop_resize_data(img, mask)
         label = mask_to_label(mask)
@@ -59,7 +60,7 @@ class LanDataSet(Dataset):
         return img, torch.from_numpy(label)
 
 
-def get_train_loader(batch_size=2):
+def get_train_loader(batch_size=1):
     return DataLoader(
         LanDataSet("data_list/train.csv"), shuffle=True, batch_size=batch_size
     )
@@ -70,4 +71,4 @@ def get_test_loader():
 
 
 def get_valid_loader():
-    return DataLoader(LanDataSet("data_list/valid.csv"), shuffle=True, batch_size=2)
+    return DataLoader(LanDataSet("data_list/valid.csv"), shuffle=True, batch_size=1)
