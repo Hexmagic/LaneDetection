a78ab9c3dd5b762fc9f0ebedcc34f5e1b8d36425
zai <zai@aliyun.com>
2020-01-04 10:04:50 +0800

    update valid

--- valid.py
+++ valid.py
@@ -1,19 +1,22 @@
-from model.deeplabv3_plus import DeeplabV3Plus
-from util.datagener import get_test_loader
 import torch
 from torch.autograd import Variable
 from torch.nn import BCELoss
+from tqdm import tqdm
+
+from model.deeplabv3_plus import DeeplabV3Plus
+from util.datagener import get_test_loader
+
 loss_fuc = BCELoss()
-net = DeeplabV3Plus(n_class=8)
-net.load_state_dict(torch.load('parameter.pkl'))
+model = DeeplabV3Plus(n_class=8)
+model.load_state_dict(torch.load('parameter.pkl'))
 test_loader = get_test_loader()
-loss_list  =[]
-for batch in  test_loader:
-	x,y = batch
-	xv,yv = Variable(x),Variable(y)
-	yout = model(xv)
-	yout = torch.sigmoid(yout)
-	loss = loss_fuc(yout,yv)
-	loss_list.append(loss.item())
+loss_list = []
+for batch in tqdm(test_loader):
+    x, y = batch
+    xv, yv = Variable(x), Variable(y)
+    yout = model(xv)
+    yout = torch.sigmoid(yout)
+    loss = loss_fuc(yout, yv)
+    loss_list.append(loss.item())
 
 print(f'Valid Losss {sum(loss_list)/len(loss_list)}')
