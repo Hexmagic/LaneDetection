adce7de6fc8586493c564e71903e4f4a263f1b2b
zai <zai@aliyun.com>
2020-01-04 10:06:07 +0800

    update valid

--- valid.py
+++ valid.py
@@ -6,14 +6,14 @@ from tqdm import tqdm
 from model.deeplabv3_plus import DeeplabV3Plus
 from util.datagener import get_test_loader
 
-loss_fuc = BCELoss()
-model = DeeplabV3Plus(n_class=8)
+loss_fuc = BCELoss().cuda()
+model = DeeplabV3Plus(n_class=8).cuda()
 model.load_state_dict(torch.load('parameter.pkl'))
 test_loader = get_test_loader()
 loss_list = []
 for batch in tqdm(test_loader):
     x, y = batch
-    xv, yv = Variable(x), Variable(y)
+    xv, yv = Variable(x).cuda(), Variable(y).cuda()
     yout = model(xv)
     yout = torch.sigmoid(yout)
     loss = loss_fuc(yout, yv)
