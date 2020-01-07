0b7c96b162d0e605bada5a523c8a8971479abbc0
Xee <191440042@qq.com>
2020-01-07 09:43:17 +0800

    'valid'

--- valid.py
+++ valid.py
@@ -5,22 +5,27 @@ from tqdm import tqdm
 
 from model.deeplabv3_plus import DeeplabV3Plus
 from util.datagener import get_test_loader
+from model.unet import Unet
 
-loss_fuc = BCELoss().cuda()
-model = DeeplabV3Plus(n_class=8).cuda()
-model.load_state_dict(torch.load('parameter.pkl'))
-test_loader = get_test_loader()
-loss_list = []
-i = 0
-for batch in tqdm(test_loader):
-	x, y = batch
-	i+=1
-	if i>100:
-		break
-	xv, yv = Variable(x).cuda(), Variable(y).cuda()
-	yout = model(xv)
-	yout = torch.sigmoid(yout)
-	loss = loss_fuc(yout, yv)
-	loss_list.append(loss.item())
+def validLoss():
+	loss_fuc = BCELoss().cuda()
+	model = Unet(n_class=8).cuda()
+	model.load_state_dict(torch.load('parameter.pkl'))
+	test_loader = get_test_loader()
+	loss_list = []
+	i = 0
+	for batch in tqdm(test_loader):
+		x, y = batch
+		i+=1
+		if i>100:
+			break
+		xv, yv = Variable(x).cuda(), Variable(y).cuda()
+		yout = model(xv)
+		yout = torch.sigmoid(yout)
+		loss = loss_fuc(yout, yv)
+		loss_list.append(loss.item())
 
-print(f'Valid Losss {sum(loss_list)/len(loss_list)}')
+	print(f'Valid Losss {sum(loss_list)/len(loss_list)}')
+
+def meanIou():
+	pass
