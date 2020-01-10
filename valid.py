0805e424c03c2b8de1684a27db61179e080fda5d
Xee <191440042@qq.com>
2020-01-10 10:11:40 +0800

    valid

--- valid.py
+++ valid.py
@@ -8,81 +8,66 @@ from util.datagener import get_test_loader
 from util.label_util import label_to_color_mask
 import sys
 from model.unet import Unet
+from util.metric import compute_iou
 from collections import defaultdict
 import numpy as np
 from visdom import Visdom
-plt  =sys.platform
+plt = sys.platform
+
 
 def encode(labels):
-	rst = []
-	for i, ele in enumerate(labels):
-		ele = np.argmax(ele, axis=0)
-		rst.append(label_to_color_mask(ele))
-	return rst
+    rst = []
+    for i, ele in enumerate(labels):
+        ele = np.argmax(ele, axis=0)
+        rst.append(label_to_color_mask(ele))
+    return rst
+
 
 if plt == 'win32':
     vis = Visdom()
 
 
-def compute_iou(pred, gt, result):
-	"""
-	pred : [N, H, W]
-	gt: [N, H, W]
-	"""
-	pred = torch.argmax(pred,dim=1)
-	gt = torch.argmax(gt,dim=1)
-	pred = pred.detach().cpu().numpy()
-	gt = gt.detach().cpu().numpy()
-	for i in range(8):
-		single_gt = gt == i
-		single_pred = pred == i
-		temp_tp = np.sum(single_gt * single_pred)
-		temp_ta = np.sum(single_pred) + np.sum(single_gt) - temp_tp
-		result["TP"][i] += temp_tp
-		result["TA"][i] += temp_ta
-	return result
-
-
 def validLoss():
-	loss_fuc = BCELoss().cuda()
-	model = torch.load('laneNet.pth').cuda()
-	test_loader = get_test_loader()
-	loss_list = []
-	i = 0
-	result = {"TP": defaultdict(int), "TA": defaultdict(int)}
-	for batch in tqdm(test_loader):
-		x, y = batch
-		i += 1
-		xv, yv = Variable(x['image']).cuda(), Variable(y).cuda()
-		yout = model(xv)
-		yout = torch.sigmoid(yout)
-		if i%5==0:
-			if plt!='win32':
-				continue
-			_np = yout.cpu().detach().numpy().copy()
-			pred = np.array(encode(_np))
-			pred = pred.transpose((0, 3, 1, 2))
-			bag_msk_np = yv.cpu().detach().numpy().copy()
-			#mask = np.argmax(bag_msk_np, axis=1)
-			label = np.array(encode(bag_msk_np))
-			bag_msk_np = label.transpose((0, 3, 1, 2))
-			vis.images(pred,
-					   win='train_pred',
-					   opts=dict(title='train prediction'))
-			vis.images(bag_msk_np,
-					   win='train_label',
-					   opts=dict(title='train prediction'))
-			vis.line(loss_list,
-					 win='train_iter_loss',
-					 opts=dict(title='train iter loss'))
-		loss = loss_fuc(yout, yv)
-		loss_list.append(loss.item())
-		result = compute_iou(yout, yv, result)
-	print(f'Valid Losss {sum(loss_list)/len(loss_list)}')
-	MIOU = 0.0
-	for i in range(8):
-		print(f"Class {i} IOU {result['TP'][i]/result['TA'][i]}")
-		MIOU += result["TP"][i] / result["TA"][i]
-	print(f"MIOU {MIOU}")
+    loss_fuc = BCELoss().cuda()
+    model = torch.load('laneNet.pth').cuda()
+    test_loader = get_test_loader()
+    loss_list = []
+    i = 0
+    result = {"TP": defaultdict(int), "TA": defaultdict(int)}
+    for batch in tqdm(test_loader):
+        x, y = batch
+        i += 1
+        xv, yv = Variable(x).cuda(), Variable(y).cuda()
+        yout = model(xv)
+        yout = torch.sigmoid(yout)
+        if i % 5 == 0:
+            if plt != 'win32':
+                continue
+            _np = yout.cpu().detach().numpy().copy()
+            pred = np.array(encode(_np))
+            pred = pred.transpose((0, 3, 1, 2))
+            bag_msk_np = yv.cpu().detach().numpy().copy()
+            #mask = np.argmax(bag_msk_np, axis=1)
+            label = np.array(encode(bag_msk_np))
+            bag_msk_np = label.transpose((0, 3, 1, 2))
+            vis.images(pred,
+                       win='train_pred',
+                       opts=dict(title='train prediction'))
+            vis.images(bag_msk_np,
+                       win='train_label',
+                       opts=dict(title='train prediction'))
+            vis.line(loss_list,
+                     win='train_iter_loss',
+                     opts=dict(title='train iter loss'))
+        loss = loss_fuc(yout, yv)
+        loss_list.append(loss.item())
+        result = compute_iou(yout, yv, result)
+    print(f'Valid Losss {sum(loss_list)/len(loss_list)}')
+    MIOU = 0.0
+    for i in range(8):
+        print(f"Class {i} IOU {result['TP'][i]/result['TA'][i]}")
+        MIOU += result["TP"][i] / result["TA"][i]
+    print(f"MIOU {MIOU}")
+
 
 validLoss()
