79a833e1c9776a8e5dad33c9ab0bd9cb2fcfd64d
Xee <191440042@qq.com>
2020-01-10 22:45:33 +0800

    updat valid

--- valid.py
+++ valid.py
@@ -17,66 +17,66 @@ plt = sys.platform
 
 torch.cuda.set_device(1)
 def encode(labels):
-    rst = []
-    for i, ele in enumerate(labels):
-        ele = np.argmax(ele, axis=0)
-        rst.append(label_to_color_mask(ele))
-    return rst
+	rst = []
+	for i, ele in enumerate(labels):
+		ele = np.argmax(ele, axis=0)
+		rst.append(label_to_color_mask(ele))
+	return rst
 
 
 if plt == 'win32':
-    vis = Visdom()
+	vis = Visdom()
 
 
 def compute_miou(result):
 	MIOU = 0.0
-    for i in range(8):
-        result_string = "{}: {:.4f} \n".format(
-            i, result["TP"][i] / result["TA"][i])
-        print(result_string)
-        MIOU += result["TP"][i] / result["TA"][i]
-    MIOU = MIOU / 8
-    return MIOU
+	for i in range(8):
+		result_string = "{}: {:.4f} \n".format(
+			i, result["TP"][i] / result["TA"][i])
+		print(result_string)
+		MIOU += result["TP"][i] / result["TA"][i]
+	MIOU = MIOU / 8
+	return MIOU
 
 
 def validLoss():
-    with torch.no_grad():
-        net = torch.load('laneNet.pth',map_location={'cuda:0':'cuda:1'})
-        net.eval()
-        total_mask_loss = []
-        dataLoader = get_test_loader(batch_size=1)
-        dataprocess = tqdm(dataLoader)
-        loss_func1 = BCEWithLogitsLoss().cuda()
-        loss_func2 = DiceLoss().cuda()
-        result = {
-            "TP": {i: 0
-                   for i in range(8)},
-            "TA": {i: 0
-                   for i in range(8)}
-        }
-        i = 0
-        for batch_item in dataprocess:
-            image, mask = batch_item
-            if torch.cuda.is_available():
-                image, mask = Variable(image).cuda(), Variable(mask, ).cuda()
-            out = net(image)
-            sig = torch.sigmoid(out)
-            loss1 = loss_func1(out, mask)
-            loss2 = loss_func2(sig, mask)
-            #loss2 = DiceLoss()(out, mask)
-            mask_loss = loss1 + loss2
-            #mask_loss = MySoftmaxCrossEntropyLoss(nbclasses=8)(out, mask.long())
-            total_mask_loss.append(mask_loss.detach().item())
-            pred = torch.argmax(F.softmax(out, dim=1), dim=1)
-            mask = torch.argmax(F.softmax(mask, dim=1), dim=1)
-            result = compute_iou(pred, mask, result)
-            dataprocess.set_postfix_str("mask_loss:{:.4f}".format(
-                np.mean(total_mask_loss)))
-            if i % 10 == 0:
-                miou = compute_miou(result)
-                dataprocess.set_description_str("MIOU:{}".format(miou))
-        miou = compute_miou(result)
-        print(f"Mean IOU {miou}")
+	with torch.no_grad():
+		net = torch.load('laneNet.pth',map_location={'cuda:0':'cuda:1'})
+		net.eval()
+		total_mask_loss = []
+		dataLoader = get_test_loader(batch_size=1)
+		dataprocess = tqdm(dataLoader)
+		loss_func1 = BCEWithLogitsLoss().cuda()
+		loss_func2 = DiceLoss().cuda()
+		result = {
+			"TP": {i: 0
+				   for i in range(8)},
+			"TA": {i: 0
+				   for i in range(8)}
+		}
+		i = 0
+		for batch_item in dataprocess:
+			image, mask = batch_item
+			if torch.cuda.is_available():
+				image, mask = Variable(image).cuda(), Variable(mask, ).cuda()
+			out = net(image)
+			sig = torch.sigmoid(out)
+			loss1 = loss_func1(out, mask)
+			loss2 = loss_func2(sig, mask)
+			#loss2 = DiceLoss()(out, mask)
+			mask_loss = loss1 + loss2
+			#mask_loss = MySoftmaxCrossEntropyLoss(nbclasses=8)(out, mask.long())
+			total_mask_loss.append(mask_loss.detach().item())
+			pred = torch.argmax(F.softmax(out, dim=1), dim=1)
+			mask = torch.argmax(F.softmax(mask, dim=1), dim=1)
+			result = compute_iou(pred, mask, result)
+			dataprocess.set_postfix_str("mask_loss:{:.4f}".format(
+				np.mean(total_mask_loss)))
+			if i % 10 == 0:
+				miou = compute_miou(result)
+				dataprocess.set_description_str("MIOU:{}".format(miou))
+		miou = compute_miou(result)
+		print(f"Mean IOU {miou}")
 
 
 validLoss()
