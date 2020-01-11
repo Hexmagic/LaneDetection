d1d02c58aef98f9aae3a0f9537e714bf9554ca84
Xee <191440042@qq.com>
2020-01-11 23:48:32 +0800

    udpate tarin:

--- train.py
+++ train.py
@@ -16,168 +16,170 @@ from model.deeplabv3_plus import DeeplabV3Plus
 from model.deeplabv3p import DeepLabV3P
 from util.creat_csv import dump
 from util.datagener import (get_test_loader, get_train_loader,
-                            get_valid_loader, one_hot)
+							get_valid_loader, one_hot)
 from util.gpu import wait_gpu
 from util.label_util import label_to_color_mask
 from util.loss import DiceLoss, FocalLoss
 from util.metric import compute_iou
 
 plt = sys.platform
-
-ava_gpu_index = wait_gpu(need=7)
-
-ids = [ava_gpu_index]
+argv = sys.argv
+if len(argv) > 2:
+	ids = int(argv[1])
+else:
+	ava_gpu_index = wait_gpu(need=7)
+	ids = [ava_gpu_index]
 
 
 def encode(labels):
-    rst = []
-    for ele in labels:
-        ele = np.argmax(ele, axis=0)
-        rst.append(label_to_color_mask(ele))
-    return rst
+	rst = []
+	for ele in labels:
+		ele = np.argmax(ele, axis=0)
+		rst.append(label_to_color_mask(ele))
+	return rst
 
 
 if plt == 'win32':
-    vis = Visdom()
+	vis = Visdom()
 
 
 def train_epoch(net, epoch, dataLoader, optimizer, trainF):
-    net.train()
-    total_mask_loss = []
-    dataprocess = tqdm(dataLoader)
-    loss_func1 = BCEWithLogitsLoss().cuda(device=ids[0])
-    loss_func2 = DiceLoss().cuda(device=ids[0])
-    #loss_func3 = FocalLoss().cuda(device=ids[0])
-    #loss_func2 = FocalLoss(class_num=8).cuda()
-    i = 0
-    dataprocess.set_description_str("epoch:{}".format(epoch))
-    for batch_item in dataprocess:
-        i += 1
-        image, mask = batch_item
-        if torch.cuda.is_available():
-            image, mask = Variable(image).cuda(
-                device=ids[0]), Variable(mask).cuda(device=ids[0])
-        optimizer.zero_grad()
-        out = net(image)
-        sig = torch.sigmoid(out)
-        loss1 = loss_func1(out, mask)  # bcewithlogitsloss
-        loss2 = loss_func2(sig, mask)
-        #loss3 = loss_func3(out, mask)
-        #loss2 = DiceLoss()(out, mask)
-        mask_loss = loss1 + loss2
-        mask_loss.backward()
-        if i % 100 == 0:
-            trainF.flush()
-        if i % 10 == 0:
-            if plt != 'win32':
-                continue
-            _np = sig.cpu().detach().numpy().copy(
-            )  # output_np.shape (4, 2, 160, 160)
-            #output_np = np.argmax(_np, axis=1)
-            pred = np.array(encode(_np))
-            pred = pred.transpose((0, 3, 1, 2))
+	net.train()
+	total_mask_loss = []
+	dataprocess = tqdm(dataLoader)
+	loss_func1 = BCEWithLogitsLoss().cuda(device=ids[0])
+	loss_func2 = DiceLoss().cuda(device=ids[0])
+	#loss_func3 = FocalLoss().cuda(device=ids[0])
+	#loss_func2 = FocalLoss(class_num=8).cuda()
+	i = 0
+	dataprocess.set_description_str("epoch:{}".format(epoch))
+	for batch_item in dataprocess:
+		i += 1
+		image, mask = batch_item
+		if torch.cuda.is_available():
+			image, mask = Variable(image).cuda(
+				device=ids[0]), Variable(mask).cuda(device=ids[0])
+		optimizer.zero_grad()
+		out = net(image)
+		sig = torch.sigmoid(out)
+		loss1 = loss_func1(out, mask)  # bcewithlogitsloss
+		loss2 = loss_func2(sig, mask)
+		#loss3 = loss_func3(out, mask)
+		#loss2 = DiceLoss()(out, mask)
+		mask_loss = loss1 + loss2
+		mask_loss.backward()
+		if i % 100 == 0:
+			trainF.flush()
+		if i % 10 == 0:
+			if plt != 'win32':
+				continue
+			_np = sig.cpu().detach().numpy().copy(
+			)  # output_np.shape (4, 2, 160, 160)
+			#output_np = np.argmax(_np, axis=1)
+			pred = np.array(encode(_np))
+			pred = pred.transpose((0, 3, 1, 2))
 
-            bag_msk_np = mask.cpu().detach().numpy().copy()
-            #mask = np.argmax(bag_msk_np, axis=1)
-            label = np.array(encode(bag_msk_np))
-            bag_msk_np = label.transpose((0, 3, 1, 2))
+			bag_msk_np = mask.cpu().detach().numpy().copy()
+			#mask = np.argmax(bag_msk_np, axis=1)
+			label = np.array(encode(bag_msk_np))
+			bag_msk_np = label.transpose((0, 3, 1, 2))
 
-            vis.images(pred, win='Pred', opts=dict(title='pred'))
-            vis.images(bag_msk_np, win='GroudTruth', opts=dict(title='label'))
-            vis.images(image, win='Image', opts=dict(title='colorimg'))
-            vis.line(total_mask_loss,
-                     win='train_iter_loss',
-                     opts=dict(title='train iter loss'))
-        total_mask_loss.append(mask_loss.item())
-        trainF.write(f'Epoch {epoch} loss {mask_loss.item()}\n')
-        #mask_loss.backward()
-        optimizer.step()
-        dataprocess.set_postfix_str("mask_loss:{:.7f}".format(
-            mask_loss.item()))
-    trainF.write(f"Epoch {epoch} loss {np.mean(total_mask_loss)}")
-    trainF.flush()
+			vis.images(pred, win='Pred', opts=dict(title='pred'))
+			vis.images(bag_msk_np, win='GroudTruth', opts=dict(title='label'))
+			vis.images(image, win='Image', opts=dict(title='colorimg'))
+			vis.line(total_mask_loss,
+					 win='train_iter_loss',
+					 opts=dict(title='train iter loss'))
+		total_mask_loss.append(mask_loss.item())
+		trainF.write(f'Epoch {epoch} loss {mask_loss.item()}\n')
+		#mask_loss.backward()
+		optimizer.step()
+		dataprocess.set_postfix_str("mask_loss:{:.7f}".format(
+			mask_loss.item()))
+	trainF.write(f"Epoch {epoch} loss {np.mean(total_mask_loss)}")
+	trainF.flush()
 
 
 def test(net, epoch, dataLoader, testF):
-    net.eval()
-    total_mask_loss = []
-    dataprocess = tqdm(dataLoader)
-    loss_func1 = BCEWithLogitsLoss().cuda(device=ids[0])
-    loss_func2 = DiceLoss().cuda(device=ids[0])
-    MIOU = 0.0
-    result = {"TP": {i: 0 for i in range(8)}, "TA": {i: 0 for i in range(8)}}
-    for batch_item in dataprocess:
-        image, mask = batch_item
-        if torch.cuda.is_available():
-            image, mask = Variable(image).cuda(
-                device=ids[0]), Variable(mask).cuda(device=ids[0])
-        out = net(image)
-        sig = torch.sigmoid(out)
-        loss1 = loss_func1(out, mask)
-        loss2 = loss_func2(sig, mask)
-        #loss2 = DiceLoss()(out, mask)
-        mask_loss = loss1 + loss2
-        #mask_loss = MySoftmaxCrossEntropyLoss(nbclasses=8)(out, mask.long())
-        total_mask_loss.append(mask_loss.detach().item())
-        pred = torch.argmax(F.softmax(out, dim=1), dim=1)
-        mask = torch.argmax(F.softmax(mask, dim=1), dim=1)
-        result = compute_iou(pred, mask, result)
-        dataprocess.set_description_str("epoch:{}".format(epoch))
-        dataprocess.set_postfix_str("mask_loss:{:.4f}".format(
-            np.mean(total_mask_loss)))
-        testF.write(f'Epoch {epoch} loss {mask_loss.item()}\n')
-    for i in range(1, 8):
-        result_string = "{}: {:.4f} \n".format(
-            i, result["TP"][i] / result["TA"][i])
-        testF.write(f'Epoch {epoch} IOU {result_string}')
-        print(result_string)
-        MIOU += result["TP"][i] / result["TA"][i]
-    testF.flush()
-    return MIOU / 7
+	net.eval()
+	total_mask_loss = []
+	dataprocess = tqdm(dataLoader)
+	loss_func1 = BCEWithLogitsLoss().cuda(device=ids[0])
+	loss_func2 = DiceLoss().cuda(device=ids[0])
+	MIOU = 0.0
+	result = {"TP": {i: 0 for i in range(8)}, "TA": {i: 0 for i in range(8)}}
+	for batch_item in dataprocess:
+		image, mask = batch_item
+		if torch.cuda.is_available():
+			image, mask = Variable(image).cuda(
+				device=ids[0]), Variable(mask).cuda(device=ids[0])
+		out = net(image)
+		sig = torch.sigmoid(out)
+		loss1 = loss_func1(out, mask)
+		loss2 = loss_func2(sig, mask)
+		#loss2 = DiceLoss()(out, mask)
+		mask_loss = loss1 + loss2
+		#mask_loss = MySoftmaxCrossEntropyLoss(nbclasses=8)(out, mask.long())
+		total_mask_loss.append(mask_loss.detach().item())
+		pred = torch.argmax(F.softmax(out, dim=1), dim=1)
+		mask = torch.argmax(F.softmax(mask, dim=1), dim=1)
+		result = compute_iou(pred, mask, result)
+		dataprocess.set_description_str("epoch:{}".format(epoch))
+		dataprocess.set_postfix_str("mask_loss:{:.4f}".format(
+			np.mean(total_mask_loss)))
+		testF.write(f'Epoch {epoch} loss {mask_loss.item()}\n')
+	for i in range(1, 8):
+		result_string = "{}: {:.4f} \n".format(
+			i, result["TP"][i] / result["TA"][i])
+		testF.write(f'Epoch {epoch} IOU {result_string}')
+		print(result_string)
+		MIOU += result["TP"][i] / result["TA"][i]
+	testF.flush()
+	return MIOU / 7
 
 
 def adjust_lr(optimizer, epoch):
-    if epoch == 0:
-        lr = 6e-4
-    elif epoch == 1:
-        lr = 4e-4
-    elif epoch == 5:
-        lr = 3e-4
-    elif epoch == 10:
-        lr = 2e-4
-    elif epoch == 15:
-        lr = 7e-5
-    else:
-        return
-    for param_group in optimizer.param_groups:
-        param_group['lr'] = lr
+	if epoch == 0:
+		lr = 6e-4
+	elif epoch == 1:
+		lr = 4e-4
+	elif epoch == 5:
+		lr = 3e-4
+	elif epoch == 10:
+		lr = 2e-4
+	elif epoch == 15:
+		lr = 7e-5
+	else:
+		return
+	for param_group in optimizer.param_groups:
+		param_group['lr'] = lr
 
 
 def main():
-    train_data_batch = get_train_loader(batch_size=2)
-    val_data_batch = get_valid_loader()
-    if os.path.exists('laneNet.pth'):
-        net = torch.load('laneNet.pth',
-                         map_location={'cuda:0': f'cuda:{ids[0]}'})
-    else:
-        net = DeeplabV3Plus(n_class=8).cuda(device=ids[0])
-    #net = DataParallel(net, device_ids=ids)
-    # optimizer = torch.optim.SGD(net.parameters(), lr=lane_config.BASE_LR,
-    #                             momentum=0.9, weight_decay=lane_config.WEIGHT_DECAY)
-    optimizer = torch.optim.Adam(net.parameters())
-    last_MIOU = 0.0
-    trainF = open('train.txt', 'w+')
-    testF = open('test.txt', 'w+')
-    for epoch in range(40):
-        adjust_lr(optimizer, epoch)
-        train_epoch(net, epoch, train_data_batch, optimizer, trainF)
-        with torch.no_grad():
-            miou = test(net, epoch, val_data_batch, testF)
-        if miou > last_MIOU:
-            print(f"miou {miou} > last_MIOU {last_MIOU},save model")
-            torch.save(net, os.path.join(os.getcwd(), "laneNet.pth"))
-            last_MIOU = miou
+	train_data_batch = get_train_loader(batch_size=2)
+	val_data_batch = get_valid_loader()
+	if os.path.exists('laneNet.pth'):
+		net = torch.load('laneNet.pth',
+						 map_location={'cuda:0': f'cuda:{ids[0]}'})
+	else:
+		net = DeeplabV3Plus(n_class=8).cuda(device=ids[0])
+	#net = DataParallel(net, device_ids=ids)
+	# optimizer = torch.optim.SGD(net.parameters(), lr=lane_config.BASE_LR,
+	#                             momentum=0.9, weight_decay=lane_config.WEIGHT_DECAY)
+	optimizer = torch.optim.Adam(net.parameters())
+	last_MIOU = 0.0
+	trainF = open('train.txt', 'w+')
+	testF = open('test.txt', 'w+')
+	for epoch in range(40):
+		adjust_lr(optimizer, epoch)
+		train_epoch(net, epoch, train_data_batch, optimizer, trainF)
+		with torch.no_grad():
+			miou = test(net, epoch, val_data_batch, testF)
+		if miou > last_MIOU:
+			print(f"miou {miou} > last_MIOU {last_MIOU},save model")
+			torch.save(net, os.path.join(os.getcwd(), "laneNet.pth"))
+			last_MIOU = miou
 
 
 if __name__ == "__main__":
-    main()
+	main()
