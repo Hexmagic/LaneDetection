cb5a80c7c367b5b3b3a69f1c8d933dcc9c63ea80
Xee <191440042@qq.com>
2020-01-20 21:49:34 +0800

    Update unet_plus.py

--- model/unet_plus.py
+++ model/unet_plus.py
@@ -517,28 +517,27 @@ class conv_block_nested(nn.Module):
         super(conv_block_nested, self).__init__()
         layers = [
             nn.Conv2d(in_channel,
-                   in_channel,
-                   kernel_size=3,
-                   padding=padding,
-                   groups=in_channel,
-                   stride=stride,
-                   dilation=dilation,
-                   bias=False),
-           nn.Conv2d(in_channel, mid_channel, kernel_size=1),
-           nn.BatchNorm2d(mid_channel), nn.ReLU(True),
-           nn.Conv2d(mid_channel,
-           mid_channel,
-           kernel_size=3,
-           padding=padding,
-           groups=mid_channel,
-           stride=stride,
-           dilation=dilation,
-           bias=False),
-           nn.Conv2d(mid_channel, out_channel, kernel_size=1),
-           nn.BatchNorm2d(out_channel), nn.ReLU(True),
-
+                      mid_channel,
+                      kernel_size=3,
+                      padding=padding,
+                      groups=1,
+                      stride=stride,
+                      dilation=dilation,
+                      bias=False),
+            nn.BatchNorm2d(mid_channel),
+            nn.ReLU(True),
+            nn.Conv2d(mid_channel,
+                      out_channel,
+                      kernel_size=3,
+                      padding=padding,
+                      groups=1,
+                      stride=stride,
+                      dilation=dilation,
+                      bias=False),
+            nn.BatchNorm2d(out_channel),
+            nn.ReLU(True),
         ]
-       
+
         self.net = nn.Sequential(*layers)
 
     def forward(self, x):
@@ -553,7 +552,7 @@ class NestedUNet(nn.Module):
     Implementation of this paper:
     https://arxiv.org/pdf/1807.10165.pdf
     """
-    def __init__(self,n_class=8,n1=16):
+    def __init__(self, n_class=8, n1=16):
         super(NestedUNet, self).__init__()
         in_ch = 3
         filters = [n1, n1 * 2, n1 * 4, n1 * 8, n1 * 16]
@@ -603,24 +602,26 @@ class NestedUNet(nn.Module):
 
         x0_0 = self.conv0_0(x)
         x1_0 = self.conv1_0(self.pool(x0_0))
-        x0_1 = self.conv0_1(torch.cat([x0_0, self.up(x1_0,x0_0)], 1))
+        x0_1 = self.conv0_1(torch.cat([x0_0, self.up(x1_0, x0_0)], 1))
 
         x2_0 = self.conv2_0(self.pool(x1_0))
-        x1_1 = self.conv1_1(torch.cat([x1_0, self.up(x2_0,x1_0)], 1))
-        x0_2 = self.conv0_2(torch.cat([x0_0, x0_1, self.up(x1_1,x0_0)], 1))
+        x1_1 = self.conv1_1(torch.cat([x1_0, self.up(x2_0, x1_0)], 1))
+        x0_2 = self.conv0_2(torch.cat([x0_0, x0_1, self.up(x1_1, x0_0)], 1))
 
         x3_0 = self.conv3_0(self.pool(x2_0))
-        x2_1 = self.conv2_1(torch.cat([x2_0, self.up(x3_0,x2_0)], 1))
-        x1_2 = self.conv1_2(torch.cat([x1_0, x1_1, self.up(x2_1,x1_0)], 1))
-        x0_3 = self.conv0_3(torch.cat([x0_0, x0_1, x0_2, self.up(x1_2,x0_0)], 1))
+        x2_1 = self.conv2_1(torch.cat([x2_0, self.up(x3_0, x2_0)], 1))
+        x1_2 = self.conv1_2(torch.cat([x1_0, x1_1, self.up(x2_1, x1_0)], 1))
+        x0_3 = self.conv0_3(
+            torch.cat([x0_0, x0_1, x0_2, self.up(x1_2, x0_0)], 1))
 
         x4_0 = self.conv4_0(self.pool(x3_0))
-        x3_1 = self.conv3_1(torch.cat([x3_0, self.up(x4_0,x3_0)], 1))
-        x2_2 = self.conv2_2(torch.cat([x2_0, x2_1, self.up(x3_1,x2_0)], 1))
-        x1_3 = self.conv1_3(torch.cat([x1_0, x1_1, x1_2, self.up(x2_2,x1_0)], 1))
+        x3_1 = self.conv3_1(torch.cat([x3_0, self.up(x4_0, x3_0)], 1))
+        x2_2 = self.conv2_2(torch.cat([x2_0, x2_1, self.up(x3_1, x2_0)], 1))
+        x1_3 = self.conv1_3(
+            torch.cat([x1_0, x1_1, x1_2, self.up(x2_2, x1_0)], 1))
         x0_4 = self.conv0_4(
-            torch.cat(
-                [x0_0, x0_1, x0_2, x0_3, self.up(x1_3,x0_0)], 1))
+            torch.cat([x0_0, x0_1, x0_2, x0_3,
+                       self.up(x1_3, x0_0)], 1))
 
         output = self.final(x0_4)
         return output
